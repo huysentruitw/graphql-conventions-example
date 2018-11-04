03d824ec637ed425634b4575f73efd28e012cf26
huysentruitw <wouter@huysentruit.be>
2018-11-04 12:09:07 +0100

    Add AddTodo mutation

--- Graph/Schema/Mutation.cs
+++ Graph/Schema/Mutation.cs
@@ -1,6 +1,19 @@
+using GraphApp.Repositories;
+using GraphQL.Conventions;
+
 namespace GraphApp.Graph.Schema
 {
     internal sealed class Mutation
     {
+        [Description("Add a TODO to the in-memory repository")]
+        public Todo AddTodo([Inject] ITodoRepository repository, [Description("The TODO description")] NonNull<string> description)
+        {
+            var model = repository.Add(description);
+            return new Todo
+            {
+                Id = model.Id,
+                Description = model.Description
+            };
+        }
     }
 }
---
+++ Graph/Schema/Todo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using GraphQL.Conventions;
+
+namespace GraphApp.Graph.Schema
+{
+    [Description("Represents a single TODO")]
+    internal sealed class Todo
+    {
+        [Description("The unique TODO identifier")]
+        public Guid Id { get; set; }
+        [Description("The TODO description")]
+        public string Description { get; set; }
+    }
+}
