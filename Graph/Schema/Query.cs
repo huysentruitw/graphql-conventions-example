c685ed51eebd5b4c8952c40b3a2bf004c9bc5dcc
huysentruitw <wouter@huysentruit.be>
2018-11-04 18:05:20 +0100

    Add queries

--- Graph/Schema/Query.cs
+++ Graph/Schema/Query.cs
@@ -1,6 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using GraphApp.Repositories;
+using GraphQL.Conventions;
+
 namespace GraphApp.Graph.Schema
 {
     internal sealed class Query
     {
+        public IEnumerable<Todo> Todos([Inject] ITodoRepository repository)
+            => repository.GetAll().Select(Map);
+
+        public Todo Todo([Inject] ITodoRepository repository, Guid id)
+            => Map(repository.Find(id));
+
+        private static Todo Map(TodoModel model)
+            => model == null ? null : new Todo
+            {
+                Id = model.Id,
+                Description = model.Description
+            };
     }
 }
